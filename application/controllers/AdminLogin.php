20ba6963fda5b613daa0ea0153a42cfb5ba6f911
Krishnendu Dalui <krishnendudalui@gmail.com>
2023-08-01 17:23:24 +0530

    adminlogin,admin,home task done

--- application/controllers/AdminLogin.php
+++ application/controllers/AdminLogin.php
@@ -8,16 +8,16 @@ Public function index(){
     $this-> load->view('admin_login');
   //  $this-> load->view('admin/include/footer.php');
 }
-public function login(){
-  $data = $this ->input->post();
-  $resp = $this->CM->select_data('lms_admin','*',$data);
-  if(count($resp)>0){
-echo json_encode(array)
-  }
+// public function login(){
+//   $data = $this ->input->post();
+//   $resp = $this->CM->select_data('lms_admin','*',$data);
+//   if(count($resp)>0){
+// echo json_encode(array)
+//   }
 
 }
 
-}
+// }
 
 
 ?>
--- assets/frontend/js/admin_login.js
+++ assets/frontend/js/admin_login.js
@@ -1,22 +1,22 @@
 $(document).on('click','#loginbutton',function(){
+    alert(email)
+//     let email = $('#email').val();
+//     let password = $('#password').val();
+//    // alert(email)
+//     if (email==''){
+//         alert("Please Enter Your Email!");
+//     }else if (password==''){
+//         alert("Please Enter Your Password")
     
-    let email = $('#email').val();
-    let password = $('#password').val();
-   // alert(email)
-    if (email==''){
-        alert("Please Enter Your Email!");
-    }else if (password==''){
-        alert("Please Enter Your Password")
+//     }else{
+//     let obj = {
+//         email:email,
+//         password:password,
+//     };
+//     $.post(BASE_URL+"adminlogin/login",obj,function(resp){
+//      console.log(resp);
+//     })
     
-    }else{
-    let obj = {
-        email:email,
-        password:password,
-    };
-    $.post(BASE_URL+"adminlogin/login",obj,function(resp){
-     console.log(resp);
-    })
-    
-    }
+//     }
 
 });
